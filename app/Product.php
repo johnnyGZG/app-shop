96c3fd6ae424a351a36ff9b70f71148088def647
programador3d <programador3d@3dstudioweb.com>
2018-04-21 22:25:53 -0500

    Comprobando imagen para mostrar en el listado Creando Accesso en el modelo Product

--- app/Product.php
+++ app/Product.php
@@ -19,4 +19,25 @@ class Product extends Model
     	// Un Producto tiene muchas imagenes
     	return $this->hasMany(product_image::class);
     }
+
+    public function getFeaturedImageUrlAttribute()
+    {
+        // Busca en la relacion con la tabla products_image si es destacada en el producto
+        $featuredImage = $this->images()->where('featured', true)->first();
+        if (!$featuredImage)
+        {
+            // Si no hay destacado definido, devuelve el primer registro encontrado
+            $featuredImage = $this->images()->first();
+        }
+
+        if ($featuredImage)
+        {
+            // si existe destacado devuelve url de la imagen
+            // url es un campo calculado definido del modelo product_image
+            return $featuredImage->url;
+        }
+
+        // default
+        return public_path() . '/images/default_product.png';
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,8 @@
                                 <div class="team-player">
                                     <div class="card card-plain">
                                         <div class="col-md-6 ml-auto mr-auto">
-                                            <img src="{{ $product->images()->first()->image }}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                                            {{-- Campo calculado desde el modelo de product --}}
+                                            <img src="{{ $product->featured_image_url }}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
                                         </div>
                                         <h4 class="card-title">{{ $product->name }}
                                             <br>
